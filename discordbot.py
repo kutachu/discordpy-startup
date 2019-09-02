16092ba39ffed893a1dcd19c514646c031f422cc
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 01:14:41 +0900

    hello

--- discordbot.py
+++ discordbot.py
@@ -7,10 +7,16 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        return
+    
     voice = await client.join_voice_channel(client.get_channel("389799900251947012"))
 
     if message.content == ("＠jump") or message.content == ("@jump"):
         player = voice.create_ffmpeg_player('jump.mp3')
         player.start()
+
+    if message.content == ("＠kuta") or message.content == ("@kuta"):
+        await message.channel.send('Hello!')
     
 client.run(token)
