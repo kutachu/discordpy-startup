1e481f319df5ecbd41195c35f584704b8c8fb0ae
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 01:30:41 +0900

    ID heroku

--- discordbot.py
+++ discordbot.py
@@ -3,13 +3,14 @@ import os
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
+voice_id = os.environ['VOICE_CHANNEL_ID']
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
 
-    voice = await client.join_voice_channel(client.get_channel("389799900251947012"))
+    voice = await client.join_voice_channel(client.get_channel(voice_id))
     if message.content == ("＠ジャンプ"):
         player = voice.create_ffmpeg_player('jump.mp3')
         player.start()
