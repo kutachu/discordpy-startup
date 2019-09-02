19a7a19409a6ba51d96d6f5d4018245ce12f2a36
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 00:53:19 +0900

    ぴんｇ

--- discordbot.py
+++ discordbot.py
@@ -2,9 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='{@, ＠}')
+bot = commands.Bot(command_prefix='＠')
 token = os.environ['DISCORD_BOT_TOKEN']
-
 
 @bot.event
 async def on_command_error(ctx, error):
