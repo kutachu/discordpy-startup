61c6a5e526bad8df01e657726ad979917b2dcf1c
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 03:03:46 +0900

    import VoiceClient

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,17 @@
 import discord
+from discord.voice_client import VoiceClient
+
 import os
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+voice = None
+
 @client.event
 async def on_message(message):
+    global voice
+    
     if message.author.bot:
         return
     
