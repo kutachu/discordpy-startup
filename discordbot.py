bb3bbe528af117da13a5509dbdc3085b23ece05d
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 01:23:08 +0900

    jump

--- discordbot.py
+++ discordbot.py
@@ -6,10 +6,12 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    voice = await client.join_voice_channel(client.get_channel("389799900251947012"))
+    if message.content == ("＠ジャンプ"):
+        player = voice.create_ffmpeg_player('jump.mp3')
+        player.start()
 
 client.run(token)
