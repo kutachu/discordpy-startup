8166e9663d61f48661774cd316a72df65fc1a8a7
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 02:01:06 +0900

    voice

--- discordbot.py
+++ discordbot.py
@@ -3,15 +3,14 @@ import os
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
-voice_id = os.environ['VOICE_CHANNEL_ID']
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
 
-    voice = await client.join_voice_channel(message.author.voice_channel)
-    if message.content == "＠ジャンプ":
+    if message.content == "＠ジャンプ" or message.content == "@ジャンプ":
+        voice = await client.join_voice_channel(message.author.voice_channel)
         player = voice.create_ffmpeg_player('jump.mp3')
         player.start()
 
