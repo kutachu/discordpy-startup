565c9a8315f0f71c2af2e7e9689b5f0195764240
kutachu <kutachu2007@yahoo.co.jp>
2019-09-03 01:11:18 +0900

    jump

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,16 @@
-from discord.ext import commands
+import discord
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='＠')
+client = discord.Client()
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+@client.event
+async def on_message(message):
+    voice = await client.join_voice_channel(client.get_channel("389799900251947012"))
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong!')
-
-
-bot.run(token)
+    if message.content == ("＠jump") or message.content == ("@jump"):
+        player = voice.create_ffmpeg_player('jump.mp3')
+        player.start()
+    
+client.run(token)
